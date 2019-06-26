ef11acafe6cfebe0dfad88ae4191f5ecb9549c11
WuYou&&LiGeng <15110015020@163.com>
2019-06-26 16:54:19 +0800

    6.26afternoon

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,29 @@
 //index.js
-//获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    arrayS: ['计算机类', '马院', '经管', '法律'],
+    arrayD: ['简单', '适中', '难'],
+    arrayT: ['选择', '填空', '简答', '混合'],
+    indexS: 0,
+    indexD: 0,
+    indexT: 0
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  bindSubjuctChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      indexS: e.detail.value
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  bindDegreeChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      indexD: e.detail.value
+    })
+  },
+  bindTypeChange: function(e){
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      indexT: e.detail.value
     })
   }
 })
